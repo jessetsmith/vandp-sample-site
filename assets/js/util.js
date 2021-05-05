b1ab6e195580f99e1e65b7047e74a825105ddb07
pauldeggington <pdeggington@gmail.com>
2021-05-05 15:59:13 -0700

    add href to a.link.depth-0 to make crawlable. add aria label to a tags

--- assets/js/util.js
+++ assets/js/util.js
@@ -15,11 +15,12 @@
 			var	$this = $(this),
 				indent = Math.max(0, $this.parents('li').length - 1),
 				href = $this.attr('href'),
-				target = $this.attr('target');
+				target = $this.attr('target'),
+				aria = $this.attr('aria-label');
 
 			b.push(
 				'<a ' +
-					'class="link depth-' + indent + '"' +
+					'class="link depth-' + indent + '"' + '' + 'aria-label=' + aria + 
 					( (typeof target !== 'undefined' && target != '') ? ' target="' + target + '"' : '') +
 					( (typeof href !== 'undefined' && href != '') ? ' href="' + href + '"' : '') +
 				'>' +
