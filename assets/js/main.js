7be4de1b924a6a8f1c204ad391b44b8d62ebf5ad
pauldeggington <pdeggington@gmail.com>
2021-05-06 23:25:54 -0700

    add cookies bar to all pages, add cookies.js file,

--- assets/js/main.js
+++ assets/js/main.js
@@ -160,27 +160,7 @@ video.addEventListener('click', function pauseVideo() {
 })
 
 
-// Cookies Toast Script
 
-$('.accept-button').click(function () {
-	$('.cookie-container').fadeOut(500); //fade modal out
-	$('#cookie-toast-1').delay(800).fadeIn(500).delay(1000).fadeOut(500);//dealy, fade toast in, delay, fade toast out
-});
-
-// Cookies Set and Get Script
-
-		// get the element
-		var accept_button = document.getElementById('accept-button');	
-		var modal_cookie = Cookies.get('V&P_consent'); // set the cookie to variable
-
-		// if click add a cookie and set the value to true
-		accept_button.addEventListener("click", () => {
-			Cookies.set('V&P_consent', 'true', { path: '/' });
-		});
-
-		if (modal_cookie) {
-			(document.getElementById('cookie-container').remove()); //remove modal
-		}
 
 
 
